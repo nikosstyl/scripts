f1dd3f87f5d168b822493ddeb0b94abbb305839c
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2021-09-16 19:56:29 +0300

    Delete rclone_write.sh

--- rclone_write.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location
-mv /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location_temp /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location
