617eaaf9e8956ebb85817acd5e29674a280217b0
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2021-09-16 19:57:19 +0300

    Update back_rclone.bat

--- back_rclone.bat
+++ back_rclone.bat
@@ -1,7 +1,7 @@
 
 start /b rclone mount UTh: "C:\Users\Nikos Stylianou\.temp_google_drive_uth"
-awsl exec "./$(pwd)/rclone_multi.sh before"
+awsl exec "./$(pwd)/rclone_multi.sh" before
 cmd /k "C:\Users\Nikos Stylianou\.backhelp\wrapper.bat"
-wsl exec "$(pwd)/rclone_multi.sh after"
+wsl exec "$(pwd)/rclone_multi.sh" after
 wsl --shutdown
 taskkill /IM rclone.exe /f
