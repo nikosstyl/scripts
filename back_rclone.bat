4121a08ae89dfec826f2dcfa56ce1ad14015350b
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2021-09-16 19:58:00 +0300

    Update back_rclone.bat

--- back_rclone.bat
+++ back_rclone.bat
@@ -1,4 +1,4 @@
-
+@ECHO OFF
 start /b rclone mount UTh: "C:\Users\Nikos Stylianou\.temp_google_drive_uth"
 awsl exec "./$(pwd)/rclone_multi.sh" before
 cmd /k "C:\Users\Nikos Stylianou\.backhelp\wrapper.bat"
