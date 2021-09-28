03ff4b1ad85e98d3c000d22ffc13ed00c695d778
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2021-09-28 12:24:36 +0300

    Scripts v.1.2

--- back_rclone.bat
+++ back_rclone.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
-start /b rclone mount UTh: "C:\Users\Nikos Stylianou\.temp_google_drive_uth"
-awsl exec "./$(pwd)/rclone_multi.sh" before
-cmd /k "C:\Users\Nikos Stylianou\.backhelp\wrapper.bat"
-wsl exec "$(pwd)/rclone_multi.sh" after
+start /b rclone mount UTh: "C:/Users/%USERNAME%/.temp_google_drive_uth"
+wsl exec "./rclone_multi.sh" before
+ping 127.0.0.1 -n 6 > nul
+cmd /k "C:\Users\%Nikos Stylianou%\.backhelp\wrapper.bat"
+wsl exec "./rclone_multi.sh" after
 wsl --shutdown
 taskkill /IM rclone.exe /f
--- rclone_multi.sh
+++ rclone_multi.sh
@@ -2,15 +2,18 @@
 
 if [ $# -ne 1 ]
 	then
+	echo "No arguments"
 	exit
 fi
 if [ "$1" == 'before' ]
 	then
-	cp /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location_temp
-	truncate -s 0 /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location
-	echo "C:/Users/Nikos Stylianou/.temp_google_drive_uth/UTh.zip" > /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location
+	echo -n "Give Username: "
+	read username
+	cp /mnt/c/Users/$username/.backhelp/default.location /mnt/c/Users/$username/.backhelp/default.location_temp
+	truncate -s 0 /mnt/c/Users/$username/.backhelp/default.location
+	echo "C:/Users/$username/.temp_google_drive_uth/UTh.zip" > /mnt/c/Users/$username/.backhelp/default.location
 	elif [ "$1" == 'after' ]
 	then
-	rm /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location
-	mv /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location_temp /mnt/c/Users/Nikos\ Stylianou/.backhelp/default.location
+	rm /mnt/c/Users/$username/.backhelp/default.location
+	mv /mnt/c/Users/$username/.backhelp/default.location_temp /mnt/c/Users/$username/.backhelp/default.location
 fi
