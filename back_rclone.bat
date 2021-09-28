0989c432491c9696f784770d9af54b8d4dd4a840
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2021-09-28 12:25:45 +0300

    Scripts v.1.3

--- back_rclone.bat
+++ back_rclone.bat
@@ -2,7 +2,7 @@
 start /b rclone mount UTh: "C:/Users/%USERNAME%/.temp_google_drive_uth"
 wsl exec "./rclone_multi.sh" before
 ping 127.0.0.1 -n 6 > nul
-cmd /k "C:\Users\%Nikos Stylianou%\.backhelp\wrapper.bat"
+cmd /k "C:\Users\%USERNAME%\.backhelp\wrapper.bat"
 wsl exec "./rclone_multi.sh" after
 wsl --shutdown
 taskkill /IM rclone.exe /f
