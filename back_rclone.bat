dfb67227d266e91bbb01a44ef0b87fadeadb61f8
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2021-09-28 12:27:10 +0300

    Delete back_rclone.bat

--- back_rclone.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-start /b rclone mount UTh: "C:/Users/%USERNAME%/.temp_google_drive_uth"
-wsl exec "./rclone_multi.sh" before
-ping 127.0.0.1 -n 6 > nul
-cmd /k "C:\Users\%USERNAME%\.backhelp\wrapper.bat"
-wsl exec "./rclone_multi.sh" after
-wsl --shutdown
-taskkill /IM rclone.exe /f
