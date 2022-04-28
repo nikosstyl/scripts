b9c5d553cb058998a61d140090d9a51a1bb1fb3e
Nikos Stylianou <stylianou404@gmail.com>
2022-04-28 21:07:33 +0300

    Cleanup branch

--- raspb-pi-zero-w/send_notification.py
+++
@@ -1,9 +0,0 @@
-import sys
-from ifttt_webhook import IftttWebhook
-
-if len(sys.argv) != 2:
-	exit()
-
-key_l='/etc/ifttt_key.conf'
-ifttt=IftttWebhook(key_l)
-ifttt.trigger('laptop_not_found', value1=sys.argv[1])
