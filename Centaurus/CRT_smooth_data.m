519429c69a729bf50d8700ac99581b9006a744fd
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2022-11-02 23:54:34 +0200

    Updated function to work with GEMS Pro sw

--- Centaurus/CRT_smooth_data.m
+++ Centaurus/CRT_smooth_data.m
@@ -2,19 +2,20 @@ function [ret_data] = CRT_smooth_data(return_data)
 % CRT_smooth_data Smooths accelerometer & gyro data recorded from Logger.
 % Saves all cleaned data to a subfolder called 'Cleaned'
 
-    DataVariables = ["AccX_g_","AccY_g_", "AccZ_g_", "GyrX", "Gyr_y", "Gyr_z", "AccRY", "AccRX", "AccRZ", "AccFZ", "AccFX", "AccFY"];
     [fname,pname] = uigetfile('*');
-    data = readtimetable(strcat(pname, fname));
+    table = importdata(strcat(pname, fname));
+    table = array2table(table.data, "VariableNames", table.textdata(1,2:size(table.textdata,2)));
 
     % Smooth input data
-    data = smoothdata(data,"movmean","SmoothingFactor",0.6,"DataVariables",DataVariables);
+    DataVariables = ["Acc y","Acc x","Acc z","Gyr x","Gyr_y","Gyr_z","Acc R y","Acc R x","Acc R z","Acc F z","Acc F x","Acc F y"];
+    table = smoothdata(table,"movmean","SmoothingFactor",0.6,"DataVariables",DataVariables);
 
     cleaned_folder = strcat(pname,'Cleaned/');
     if isfolder(cleaned_folder) == false
         mkdir(cleaned_folder)
     end
 
-    writetimetable(data, strcat(cleaned_folder, fname));
+    writetable(table, strcat(cleaned_folder, fname));
     
     if return_data == true
         ret_data = data;
