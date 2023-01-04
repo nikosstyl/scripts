7c17bb84f5a2a5e11f39fdb2b4a13169f1d8bc6d
Nikos Stylianou <stylianou404@gmail.com>
2023-01-04 16:59:23 +0200

    MATLAB scripts: Updated to v.2

--- Centaurus/CRT_smooth_data.m
+++ Centaurus/CRT_smooth_data.m
@@ -1,14 +1,34 @@
-function [ret_data] = CRT_smooth_data(return_data)
-% CRT_smooth_data Smooths accelerometer & gyro data recorded from Logger.
-% Saves all cleaned data to a subfolder called 'Cleaned'
+function [] = CRT_smooth_data()
+% CRT_smooth_data: Smooths accelerometer & gyro data recorded from Logger.
+% Also, calculates heave and roll on each axle.
+% Saves all cleaned data to a subfolder called 'Cleaned'.
 
-    [fname,pname] = uigetfile('*');
+    [fname,pname] = uigetfile('*.csv');
+    if ~any(fname) || ~any(pname)
+        errordlg("Calculation aborted!", "CRT Data Smoother")
+        return
+    end
     table = importdata(strcat(pname, fname));
     table = array2table(table.data, "VariableNames", table.textdata(1,2:size(table.textdata,2)));
 
+    front_axle = Rotary_to_length("front", table.("DAMPER_LF [deg]"), table.("DAMPER_RF [deg]"));
+    rear_axle = Rotary_to_length("rear", table.("DAMPER_LR [deg]"), table.("DAMPER_RR [deg]"));
+    table.("Front Roll") = front_axle(:,1);
+    table.("Front Heave") = front_axle(:,2);
+    table.("Rear Roll") = rear_axle(:,1);
+    table.("Rear Heave") = rear_axle(:,2);
+
     % Smooth input data
-    DataVariables = ["Acc y","Acc x","Acc z","Gyr x","Gyr_y","Gyr_z","Acc R y","Acc R x","Acc R z","Acc F z","Acc F x","Acc F y"];
-    table = smoothdata(table,"movmean","SmoothingFactor",0.6,"DataVariables",DataVariables);
+    DataVariables.movement = ["Acc y","Acc x","Acc z","Gyr x","Gyr_y","Gyr_z",...
+        "Acc R y","Acc R x","Acc R z","Acc F z","Acc F x","Acc F y",];
+    DataVariables.dampers  = ["DAMPER_LF [deg]", "DAMPER_RF [deg]",...
+        "DAMPER_LR [deg]", "DAMPER_RR [deg]", "Front Roll", ...
+        "Front Heave", "Rear Roll", "Rear Heave"];
+
+    % Smooth acceleration data with a bigger smoothing factor.
+    table = smoothdata(table,"movmean","SmoothingFactor",0.6,"DataVariables",DataVariables.movement);
+    % Smooth damper data with a smaller smoothing factor.
+    table = smoothdata(table,"movmean","SmoothingFactor",0.35,"DataVariables",DataVariables.dampers);
 
     cleaned_folder = strcat(pname,'Cleaned/');
     if isfolder(cleaned_folder) == false
@@ -17,9 +37,4 @@ function [ret_data] = CRT_smooth_data(return_data)
 
     writetable(table, strcat(cleaned_folder, fname));
     
-    if return_data == true
-        ret_data = data;
-    else
-        ret_data = 0;
-    end
 end
--- Centaurus/Rotary_to_length.m
+++ Centaurus/Rotary_to_length.m
@@ -1,38 +1,58 @@
-function [axle] = Rotary_to_length (POT_L, POT_R)
+function [values] = Rotary_to_length (axle, POT_L, POT_R)
 	% Inputs: Columns from logger's data
-	% Return values: axle(1:) = ROLL, axle(2:) = HEAVE
+	% Return values: values(1:) = ROLL, values(2:) = HEAVE
 	arraysize = size(POT_L, 1);
-	axle = zeros(arraysize,2);
+	values = zeros(arraysize,2);
 	
 	for i=1:1:arraysize
-		tmp = Rotary_to_length_for_one(POT_L(i), POT_R(i));
-		axle(i,1) = tmp(1); % ROLL
-		axle(i,2) = tmp(2); % HEAVE
+		tmp = Rotary_to_length_for_one(POT_L(i), POT_R(i), axle);
+		values(i,1) = tmp(1); % ROLL
+		values(i,2) = tmp(2); % HEAVE
 	end
 end
 
-function [DL]=Rotary_to_length_for_one(POT_L, POT_R)
+function [DL]=Rotary_to_length_for_one(POT_L, POT_R, axle)
 	% Inputs: POT_R, POT_L poy bgazei to rotary
 	% potensiometro SE deg!!!
+    % Every length unit used is in mm
 	% Return variables: DL(1) = Dlroll, DL(2) = Dlheave;
 	
-	ycr = 155;
-	zcr = 600;
-	ycl = -155;
-	zcl = 600;
+    if axle == "front"
+	    ycr = 155;
+	    zcr = 600;
+	    ycl = -155;
+	    zcl = 600;
+    elseif axle == "rear"
+        ycr = 135;
+        ycl = -135;
+        zcr = 460; 
+        zcl = 460;
+    end
 	
-	Rrheave = 32.88;
-	Rlheave = 32.88;
-	Rrroll = 45.37;
-	Rlroll = 46.09;
+    if axle == "front"
+	    Rrheave = 32.88;
+	    Rlheave = 32.88;
+	    Rrroll = 45.37;
+	    Rlroll = 46.09;
+    elseif axle == "rear"
+        Rrheave = 41;
+	    Rlheave = 41.59;
+	    Rrroll = 58.46;
+	    Rlroll = 107.32;
+    end
 	
 	yrheave = ycr + Rrheave*cosd(POT_R);
 	zrheave = zcr + Rrheave*sind(POT_R);
 	ylheave = ycl + Rlheave*cosd(POT_L);
 	zlheave = zcl + Rlheave*sind(POT_L);
 	
-	dfr = rad2deg(2.801952); % Gwnia metaksi roll kai heave sto right rocker
-	dfl = rad2deg(-0.296182); % Gwnia metaksi roll kai heave sto left rocker
+    if axle == "front"
+	    dfr = rad2deg(2.801952); % Gwnia metaksi roll kai heave sto right rocker
+	    dfl = rad2deg(-0.296182); % Gwnia metaksi roll kai heave sto left rocker
+    elseif axle == "rear"
+        dfr = rad2deg(2.651); % Gwnia metaksi roll kai heave sto right rocker
+	    dfl = rad2deg(-0.462); % Gwnia metaksi roll kai heave sto left rocker
+    end
 	
 	yrroll = ycr + Rrroll*cosd(POT_R+dfr);
 	zrroll = zcr + Rrroll*sind(POT_R+dfr);
@@ -51,3 +71,4 @@ function [DL]=Rotary_to_length_for_one(POT_L, POT_R)
 	
 	DL = [Dlroll, Dlheave];
 end
+
