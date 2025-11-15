ebf523069342e75bd59817cb51a1584f48176c97
nikosstyl <stylianou404@gmail.com>
2025-11-15 21:18:57 +0100

    Refactor argument parsing and error handling in LTspice symbol copy script

--- ltspice_copy_symbol/copy_symbol.py
+++ ltspice_copy_symbol/copy_symbol.py
@@ -6,6 +6,10 @@ CONFIG_FILE = "config.json"
 ACCEPTED_COMPONENTS = []
 FILE_POSITIONS = []
 
+Parser = argparse.ArgumentParser(description="Simple script that copies a default LTspice symbol to a custom-made one.")
+Parser.add_argument('-c', '--component', help=f"Set the correct component to copy. Acceptable components are: {ACCEPTED_COMPONENTS}")
+Parser.add_argument('-t', '--target', help="Set the target file to copy the symbol to.")
+
 def read_accepted_components(configuration_file:str) -> tuple[list,list]:
 	ret_element_list = []
 	ret_file_list = []
@@ -17,53 +21,61 @@ def read_accepted_components(configuration_file:str) -> tuple[list,list]:
 			ret_file_list.append(element[1])
 	return(ret_element_list, ret_file_list)
 
+def check_arguments(parser: argparse.ArgumentParser) -> tuple[str,str]:
+	args = parser.parse_args()
+
+	component = args.component
+	target_file = args.target
+
+	if not component or not target_file:
+		parser.print_usage()
+		exit(1)
+
+	component = str(component).lower()
+	target_file = str(target_file).lower()
+
+	if component not in ACCEPTED_COMPONENTS:
+		print(f"Component {component} was not found in the accepted components library.")
+		print(f"Accepted components: {ACCEPTED_COMPONENTS}")
+		exit(2)
+	if not os.path.isfile(target_file):
+		print(f"Target file \"{target_file}\" does not exist!")
+		exit(5)
+	return(component, target_file)
+
 ACCEPTED_COMPONENTS, FILE_POSITIONS = read_accepted_components(CONFIG_FILE)
 
-error_found = False
-for i, comp in enumerate(FILE_POSITIONS):
-	if not os.path.isfile(LTSPICE_DEFAULT_SYMBOL_FOLDER+comp):
-		print(f"Component \"{ACCEPTED_COMPONENTS[i]}\" with filename \"{comp}\" is missing from the default LTspice symbol folder \"{LTSPICE_DEFAULT_SYMBOL_FOLDER}\"!")
-		error_found = True
+def main():
+	error_found = False
+	for i, comp in enumerate(FILE_POSITIONS):
+		if not os.path.isfile(LTSPICE_DEFAULT_SYMBOL_FOLDER+comp):
+			print(f"Component \"{ACCEPTED_COMPONENTS[i]}\" with filename \"{comp}\" is missing from the default LTspice symbol folder \"{LTSPICE_DEFAULT_SYMBOL_FOLDER}\"!")
+			error_found = True
 
-if error_found:
-	exit(4)
+	if error_found:
+		exit(4)
 
-parser = argparse.ArgumentParser(description="Simple script that copies a default LTspice symbol to a custom-made one.")
-parser.add_argument('-c', '--component', help=f"Set the correct component to copy. Acceptable components are: {ACCEPTED_COMPONENTS}")
-parser.add_argument('-t', '--target')
-args = parser.parse_args()
+	component, target_file = check_arguments(Parser)
 
-component = args.component
-target_file = args.target
+	component_filename = LTSPICE_DEFAULT_SYMBOL_FOLDER + FILE_POSITIONS[ACCEPTED_COMPONENTS.index(component)]
 
-if not component or not target_file:
-    parser.print_usage()
-    exit(1)
+	component_lines = []
+	try:
+		with open(component_filename) as file:
+			for line in file:
+				if "LINE" in line:
+					component_lines.append(line)
+	except FileNotFoundError: 
+		print(f"Component {component} is not in the default LTspice symbol folder \"{LTSPICE_DEFAULT_SYMBOL_FOLDER}\"!")
+		exit(3)
 
-component = str(component).lower()
-target_file = str(target_file).lower()
+	try:
+		with open(target_file, "a") as file:
+			for line in component_lines:
+				file.write(line)
+	except FileNotFoundError:
+		print(f"Target file {target_file} cannot be found!")
+		exit(5)
 
-if component not in ACCEPTED_COMPONENTS:
-    print(f"Component {component} was not found in the accepted components library.")
-    print(f"Accepted components: {ACCEPTED_COMPONENTS}")
-    exit(2)
-
-component_filename = LTSPICE_DEFAULT_SYMBOL_FOLDER + FILE_POSITIONS[ACCEPTED_COMPONENTS.index(component)]
-
-component_lines = []
-try:
-	with open(component_filename) as file:
-		for line in file:
-			if "LINE" in line:
-				component_lines.append(line)
-except FileNotFoundError: 
-	print(f"Component {component} is not in the default LTspice symbol folder \"{LTSPICE_DEFAULT_SYMBOL_FOLDER}\"!")
-	exit(3)
-
-try:
-    with open(target_file, "a") as file:
-        for line in component_lines:
-            file.write(line)
-except FileNotFoundError:
-	print(f"Target file {target_file} cannot be found!")
-	exit(5)
+if __name__ == "__main__":
+	main()
